4aa4a2524997b481bd30aea7c0730f419d578d60
Pierre Lejeune <darkanakin41@gmail.com>
2022-07-30 11:33:53 +0200

    fix(Docker): Execute cleanup anywhere

--- src/commands/docker/cleanup.ts
+++ src/commands/docker/cleanup.ts
@@ -1,21 +1,8 @@
 import { DockerUtils } from '../../utils/dockerUtils';
-import { ConfigManager } from '../../utils/config';
-import { Logger } from '../../utils/logger';
 
 export const command: string = 'cleanup';
 export const desc: string = 'Cleanup docker unused images and volumes';
 export const builder = {};
 export const handler = async (): Promise<void> => {
-  const data = ConfigManager.getConfiguration();
-  const projectName = data.project.name;
-
-  if (DockerUtils.isDockerized(data.project.root)) {
-    Logger.success(`[docker] ${projectName}: dockerized`);
-  } else {
-    Logger.error(`[docker] ${projectName}: not dockerized`);
-    return
-  }
-
   DockerUtils.cleanup()
-
 };
